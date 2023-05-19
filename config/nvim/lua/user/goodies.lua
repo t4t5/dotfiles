829ac074a1834c0028ca0d23713f54b6d545e043
Tristan Edwards <tristan.edwards@me.com>
2023-05-19 08:29:48 +0100

    Improve LSP

--- config/nvim/lua/user/goodies.lua
+++ config/nvim/lua/user/goodies.lua
@@ -11,3 +11,29 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+-- Set syntax highlighting for .env.local, .env.development, .env.production...
+vim.api.nvim_command("au! BufNewFile,BufRead .env.* set filetype=sh")
+vim.api.nvim_command("au! BufNewFile,BufRead zshrc set filetype=sh")
+
+-- set hbs file type to html.handlebars
+vim.api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  { pattern = { "*.hbs", "*.handlebars" }, command = "set ft=html.handlebars" }
+)
+-- no newline at end of hbs files:
+vim.api.nvim_command("autocmd FileType html.handlebars setlocal noeol binary")
+
+-- For ruby files:
+-- See: https://github.com/testdouble/standard/wiki/IDE:-neovim
+vim.opt.signcolumn = "yes" -- otherwise it bounces in and out, not strictly needed though
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  group = vim.api.nvim_create_augroup("RubyLSP", { clear = true }), -- also this is not /needed/ but it's good practice
+  callback = function()
+    vim.lsp.start {
+      name = "standard",
+      cmd = { "/opt/homebrew/opt/rbenv/shims/standardrb", "--lsp" },
+    }
+  end,
+})
--- config/nvim/lua/user/lsp.lua
+++ config/nvim/lua/user/lsp.lua
@@ -25,45 +25,13 @@ vim.keymap.set('n', '<leader>aj', vim.diagnostic.goto_next, { desc = 'Go to next
 -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, { desc = 'Hover Documentation' })
+vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
+
+
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  -- local nmap = function(keys, func, desc)
-  --   if desc then
-  --     desc = 'LSP: ' .. desc
-  --   end
-
-  --   vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  -- end
-
-  -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  -- nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  -- nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  -- nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- -- See `:help K` for why this keymap
-  -- nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- -- Lesser used LSP functionality
-  -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  -- nmap('<leader>wl', function()
-  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  -- end, '[W]orkspace [L]ist Folders')
-
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
@@ -235,6 +203,16 @@ cmp.setup {
     },
   },
 }
+
+-- Show bordered window for errors:
+vim.diagnostic.config {
+  float = { border = "rounded" },
+}
+
+-- Show bordered window for documentation (<leader>k) window:
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded",
+})
 
 local null_ls = require("null-ls")
 null_ls.setup({

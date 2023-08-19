790b2eb6d213e1150b292372198002152d437985
Tristan Edwards <tristan.edwards@me.com>
2023-08-19 09:49:43 +0200

    Remove lvim

--- config/lvim/lua/user/statusline.lua
+++
@@ -1,7 +0,0 @@
--- show filename instead of git branch:
-lvim.builtin.lualine.sections.lualine_b = {
-  { 'filename', path = 1 }
-}
-
-lvim.builtin.lualine.sections.lualine_y = {}
-lvim.builtin.lualine.sections.lualine_z = {}

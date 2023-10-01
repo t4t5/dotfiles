e14d823f5b319611be3c826e00e26116e02fd051
Tristan (t4t5) <t4t5@hey.com>
2023-10-01 18:35:12 +0100

    Simplify custom_langs

--- config/nvim/lua/user/custom_langs.lua
+++ config/nvim/lua/user/custom_langs.lua
@@ -3,11 +3,12 @@
 
 local lspconfig = require 'lspconfig'
 local configs = require 'lspconfig.configs'
-local ft = require('Comment.ft')
+local comment = require('Comment.ft')
 
 -- .env files
 vim.api.nvim_command("au! BufNewFile,BufRead .env.* set filetype=sh")
 vim.api.nvim_command("au! BufNewFile,BufRead zshrc set filetype=sh")
+vim.api.nvim_command("au! BufNewFile,BufRead *.conf set filetype=ini")
 
 -- hbs (html.handlebars)
 vim.api.nvim_create_autocmd(
@@ -17,32 +18,17 @@ vim.api.nvim_create_autocmd(
 vim.api.nvim_command("autocmd FileType html.handlebars setlocal noeol binary") -- no newline at end of hbs files
 
 -- Caddyfile
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = { "*Caddyfile" }, command = "set ft=caddyfile syntax=nginx" }
-)
+vim.api.nvim_command("au! BufNewFile,BufRead *Caddyfile set ft=caddyfile syntax=nginx")
 
 -- justfile
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = { "justfile" }, command = "set ft=justfile syntax=make" }
-)
-ft.set('justfile', '#%s') -- comments
-
+vim.api.nvim_command("au! BufNewFile,BufRead justfile set ft=justfile syntax=make")
+comment.set('justfile', '#%s')
 
 -- mdx
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = { "*.mdx" }, command = "set ft=markdown syntax=markdown" }
-)
-
+vim.api.nvim_command("au! BufNewFile,BufRead *.mdx set ft=markdown")
 
 -- Noir:
-
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = { "*.nr" }, command = "set ft=noir syntax=rust" }
-)
+vim.api.nvim_command("au! BufNewFile,BufRead *.nr set ft=noir syntax=rust")
 
 if not configs.noir_lsp then
   configs.noir_lsp = {
@@ -53,11 +39,12 @@ if not configs.noir_lsp then
     },
   }
 end
+
 lspconfig.noir_lsp.setup {}
-ft.set('noir', '//%s') -- comments
+
+comment.set('noir', '//%s')
 
 -- Cairo:
-
 if not configs.cairo_lsp then
   configs.cairo_lsp = {
     default_config = {
@@ -67,4 +54,5 @@ if not configs.cairo_lsp then
     },
   }
 end
+
 lspconfig.cairo_lsp.setup {}

f051b6906f48831498d90378dba2e2377678775d
Tristan (t4t5) <t4t5@hey.com>
2023-10-10 21:06:22 +0100

    Fix sorting of buffers

--- config/nvim/lua/user/telescope.lua
+++ config/nvim/lua/user/telescope.lua
@@ -21,7 +21,7 @@ require("telescope").load_extension("ui-select")
 pcall(require('telescope').load_extension, 'fzf')
 
 function ShowBuffers()
-  require('telescope.builtin').buffers({ sort_lastused = true, ignore_current_buffer = true })
+  require('telescope.builtin').buffers({ sort_mru = true, ignore_current_buffer = true })
 end
 
 -- Show buffers:

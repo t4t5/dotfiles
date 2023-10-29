8b2bbaed2a282dd1e45359f4e2d5662233832dae
Tristan (t4t5) <t4t5@hey.com>
2023-10-29 10:29:07 +0000

    Add svg-to-next script

---
+++ recipes/add/svg-to-next/before.sh
@@ -0,0 +1,1 @@
+bun add @svgr/webpack
---
+++ recipes/add/svg-to-next/template/next.config.js
@@ -0,0 +1,18 @@
+// next.config.js
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;

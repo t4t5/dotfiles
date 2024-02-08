348b340ea55cd7614bdbb990d2c2ed11ae01c198
Tristan (t4t5) <t4t5@hey.com>
2024-02-08 13:45:48 +0000

    Add surround plugin

--- config/nvim/lua/user/plugins/init.lua
+++ config/nvim/lua/user/plugins/init.lua
@@ -101,4 +101,14 @@ require('lazy').setup({
       }
     end
   },
+
+  -- change surrounding tags
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end
+  },
 }, {})

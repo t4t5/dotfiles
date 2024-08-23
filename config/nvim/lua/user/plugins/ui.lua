6b28bca2a56b2cab78b7a21edcaf8244c82cbe2e
Tristan (t4t5) <t4t5@hey.com>
2024-08-23 07:25:34 +0100

    Use less obtrusive indentline

--- config/nvim/lua/user/plugins/ui.lua
+++ config/nvim/lua/user/plugins/ui.lua
@@ -40,6 +40,13 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     main = "ibl",
     opts = {},
+    config = function()
+      local ibl = require("ibl")
+
+      ibl.setup({
+        indent = { char = '┊', highlight = "LineNr" }
+      })
+    end
   },
 
   {

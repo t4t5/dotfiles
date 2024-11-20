acec27c26b4c99022d0b8796e6339b3b9d0f95d3
Tristan (t4t5) <t4t5@hey.com>
2024-11-20 17:18:25 +0000

    Add todos

--- config/nvim/lua/bitcoin_script_hints/op_codes.lua
+++ config/nvim/lua/bitcoin_script_hints/op_codes.lua
@@ -21,27 +21,94 @@ local OP_CAT = require('bitcoin_script_hints.op_codes.OP_CAT')
 local OP_2DUP = require('bitcoin_script_hints.op_codes.OP_2DUP')
 local OP_ROT = require('bitcoin_script_hints.op_codes.OP_ROT')
 
+-- The opcodes follow the same order as displayed on:
+-- https://en.bitcoin.it/wiki/Script
+
 return {
-  OP_TOALTSTACK = OP_TOALTSTACK,
-  OP_DEPTH = OP_DEPTH,
+  -- Constants:
   OP_0 = OP_0,
+  -- TODO: OP_FALSE (same as OP_0)
   OP_1 = OP_1,
   OP_2 = OP_2,
-  OP_DUP = OP_DUP,
-  OP_ADD = OP_ADD,
-  OP_SUB = OP_SUB,
-  OP_1ADD = OP_1ADD,
-  OP_1SUB = OP_1SUB,
-  OP_GREATERTHAN = OP_GREATERTHAN,
-  OP_NUMEQUAL = OP_NUMEQUAL,
-  OP_NUMNOTEQUAL = OP_NUMNOTEQUAL,
+  -- TODO: OP_PUSHDATA1, PUSHDATA2, PUSHDATA4
+  -- TODO: OP_1NEGATE
+  -- TODO: OP_TRUE (same as OP_1)
+  -- OP_3-16 (works same way as OP_1, OP_2...)
+
+  -- Flow control:
+  -- TODO: OP_NOP
   OP_IF = OP_IF,
+  -- TODO: OP_NOTIF
   OP_ELSE = OP_ELSE,
   OP_ENDIF = OP_ENDIF,
+  -- TODO: OP_VERIFY
+  -- TODO: OP_RETURN
+
+  -- Stack:
+  OP_TOALTSTACK = OP_TOALTSTACK,
   OP_FROMALTSTACK = OP_FROMALTSTACK,
-  OP_SWAP = OP_SWAP,
-  OP_SHA256 = OP_SHA256,
-  OP_CAT = OP_CAT,
-  OP_2DUP = OP_2DUP,
+  -- TODO: OP_IFDUP
+  OP_DEPTH = OP_DEPTH,
+  -- TODO: OP_DROP
+  OP_DUP = OP_DUP,
+  -- TODO: OP_NIP
+  -- TODO: OP_OVER
+  -- TODO: OP_PICK
+  -- TODO: OP_ROLL
   OP_ROT = OP_ROT,
+  OP_SWAP = OP_SWAP,
+  -- TODO: OP_TUCK
+  -- TODO: OP_2DROP
+  OP_2DUP = OP_2DUP,
+  -- TODO: OP_3DUP
+  -- TODO: OP_2OVER
+  -- TODO: OP_2ROT
+  -- TODO: OP_2SWAP
+
+  -- Splice:
+  OP_CAT = OP_CAT,
+  -- TODO: OP_SIZE
+
+  -- Bitwise logic:
+  -- TODO: OP_EQUAL
+  -- TODO: OP_EQUALVERIFY
+
+  -- Arithmetic:
+  OP_1ADD = OP_1ADD,
+  OP_1SUB = OP_1SUB,
+  -- TODO: OP_NEGATE
+  -- TODO: OP_ABS
+  -- TODO: OP_NOT
+  -- TODO: OP_0NOTEQUAL
+  OP_ADD = OP_ADD,
+  OP_SUB = OP_SUB,
+  -- TODO: OP_BOOLAND
+  -- TODO: OP_BOOLOR
+  OP_NUMEQUAL = OP_NUMEQUAL,
+  -- TODO: OP_NUMEQUALVERIFY
+  OP_NUMNOTEQUAL = OP_NUMNOTEQUAL,
+  -- TODO: OP_LESSTHAN
+  OP_GREATERTHAN = OP_GREATERTHAN,
+  -- TODO: OP_LESSTHANOREQUAL
+  -- TODO: OP_GREATERTHANOREQUAL
+  -- TODO: OP_MIN
+  -- TODO: OP_MAX
+  -- TODO: OP_WITHIN
+
+  -- Crypto:
+  -- TODO: OP_RIPEMD160 (same as sha)
+  -- TODO: OP_SHA1 (same as sha)
+  OP_SHA256 = OP_SHA256,
+  -- TODO: OP_HASH160 (same as sha)
+  -- TODO: OP_HASH256 (same as sha)
+  -- TODO: OP_CODESSEPARATOR
+  -- TODO: OP_CHECKSIG
+  -- TODO: OP_CHECKSIGVERIFY
+  -- TODO: OP_CHECKMULTISIG
+  -- TODO: OP_CHECKMULTISIGVERIFY
+  -- TODO: OP_CHECKSIGADD
+
+  -- Locktime:
+  -- TODO: OP_CHECKLOCKTIMEVERIFY
+  -- TODO: OP_CHECKSEQUENCEVERIFY
 }

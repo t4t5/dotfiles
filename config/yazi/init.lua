6d249e340703f496ffde6945edcc4a3c0a632707
Tristan (t4t5) <t4t5@hey.com>
2025-11-06 10:08:55 +0000

    Set up yazi bunny

---
+++ config/yazi/init.lua
@@ -0,0 +1,17 @@
+require("bunny"):setup({
+	hops = {
+		{ key = "r", path = "/", desc = "system root" },
+		{ key = "h", path = "~", desc = "home" },
+		{ key = ".", path = "~/dev/dotfiles", desc = "dotfiles" },
+		{ key = ",", path = "~/dev", desc = "dev" },
+		{ key = "d", path = "~/Downloads", desc = "downloads" },
+		{ key = "i", path = "~/Pictures", desc = "images" },
+		{ key = "v", path = "~/Videos", desc = "videos" },
+		{ key = "s", path = "~/Documents/syncthing", desc = "syncthing" },
+	},
+	desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+	ephemeral = true, -- Enable ephemeral hops, default is true
+	tabs = true, -- Enable tab hops, default is true
+	notify = false, -- Notify after hopping, default is false
+	fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
+})

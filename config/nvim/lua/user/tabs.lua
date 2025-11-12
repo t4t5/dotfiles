3b234cca1ac6a043873839b7fe858b2d9bb1abc3
Tristan (t4t5) <t4t5@hey.com>
2025-11-12 08:56:16 +0000

    Prefer double quotes in lua

--- config/nvim/lua/user/tabs.lua
+++ config/nvim/lua/user/tabs.lua
@@ -1,16 +1,14 @@
----@diagnostic disable: missing-fields
-
 -- Bufferline: Use real vim tabs instead of all buffers:
 require("bufferline").setup {
   options = {
     mode = "tabs",
-    close_icon = '',
-    buffer_close_icon = '',
-    modified_icon = '~',
+    close_icon = "",
+    buffer_close_icon = "",
+    modified_icon = "~",
     diagnostics = "nvim_lsp",
     separator_style = "thin",
     always_show_bufferline = false,
-  }
+  },
 }
 
 -- open/close tabs
@@ -19,5 +17,5 @@ vim.api.nvim_set_keymap("n", "tx", ":tabclose<cr>", { silent = true })
 vim.api.nvim_set_keymap("n", "tb", "<C-w>T", { silent = true })
 
 -- switch between tabs with ctrl + arrow keys
-vim.keymap.set('n', "<C-Left>", ":tabprevious<cr>")
-vim.keymap.set('n', "<C-Right>", ":tabnext<cr>")
+vim.keymap.set("n", "<C-Left>", ":tabprevious<cr>")
+vim.keymap.set("n", "<C-Right>", ":tabnext<cr>")

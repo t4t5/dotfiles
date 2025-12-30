e8eb634ce44b0d3f2705f0a4485bd4128d69c829
Tristan (t4t5) <t4t5@hey.com>
2025-12-30 15:20:19 +0000

    Fix dadbod for sqlite

--- config/nvim/lua/user/plugins/ui.lua
+++ config/nvim/lua/user/plugins/ui.lua
@@ -55,7 +55,7 @@ return {
 
   -- Show color preview for hex strings
   {
-    'norcalli/nvim-colorizer.lua',
+    'catgoose/nvim-colorizer.lua',
     config = function()
       require('colorizer').setup()
     end,
